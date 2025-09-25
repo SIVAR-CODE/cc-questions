61e03c641204b47f89d28b379d28dc997fa864d5
SIVAR-CODE <sivadhanushiya0@gmail.com>
2025-09-25 14:29:35 +0530

    Add files via upload

---
+++ CCQ3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char a;
+	printf("ENTER A ALPHABET:");
+	scanf("%c, &a");
+	if("a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'a=='U'"){
+		printf("the given value is alphabet");
+	}
+	else{
+		printf("the given value is not alphabet");
+	}
+	    return 0;
+	}
