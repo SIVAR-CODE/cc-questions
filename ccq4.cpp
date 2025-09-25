d6feeb3f9bf5985440599ab0a91ded11076f173b
SIVAR-CODE <sivadhanushiya0@gmail.com>
2025-09-25 14:29:53 +0530

    Add files via upload

---
+++ ccq4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char a;
+	printf("ENTER A DIGIT:");
+	scanf("%c, &a");
+	if("a=='1'||a=='2'||a=='3'||a=='4'||a=='5'||a=='6'||a=='7'||a=='8'||a=='9'a=='10'"){
+		printf("the given value is digit");
+	}
+	else{
+		printf("the given value is not digit");
+	}
+	    return 0;
+	}
